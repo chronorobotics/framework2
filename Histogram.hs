459df071bdd5de84523c05c5b8bd27d35e90d617
Hume2 <teratux.mail@gmail.com>
2020-07-27 21:54:18 +0200

    optimise histogram

--- Histogram.hs
+++ Histogram.hs
@@ -4,18 +4,21 @@ import Data.Fixed
 import Predictor
 import Mean
 
+import Debug.Trace
+
 getBinNo :: (RealFrac t, Integral n) => t -> t -> n -> n
 getBinNo period time nbins = floor ((mod' time period) / bin_length)
     where bin_length = period / (fromIntegral nbins)
 
-histogramPredictor :: RealFrac t => [y] -> t -> String -> Predictor t y
+histogramPredictor :: (RealFrac t, Show y) => [y] -> t -> String -> Predictor t y
 histogramPredictor bins period str = (str, pred)
     where pred t = bins !! (getBinNo period t (length bins))
 
-histogramMethod :: (RealFrac t, Meanable y) => MethodWithParams (t, Int) t y
-histogramMethod (period, nbins) dat = histogramPredictor (map mean bins) period ("Hist "++(show nbins))
-    where s = replicate nbins []
-          addToBin bs n y = b1 ++ [b ++ [y]] ++ b2
+histogramMethod :: (RealFrac t, Show y, Meanable y) => MethodWithParams (t, Int) t y
+histogramMethod (period, nbins) dat = histogramPredictor (map (\(s,n) -> meanDivision s n) bins) period ("Hist "++(show nbins))
+    where s = replicate nbins (meanZero, 0)
+          addToBin bs n y = b1 ++ [add b y] ++ b2
               where (b1, (b:b2)) = splitAt n bs
+                    add (s, n) y = (meanSum s y, n + 1)
           bins = foldr (\(t, y) bs -> addToBin bs (getBinNo period t nbins) y) s dat
 
--- Mean.hs
+++ Mean.hs
@@ -5,13 +5,24 @@ module Mean where
 import Predictor
 
 class Meanable a where
+    meanZero :: a
+    meanSum :: a -> a -> a
+    meanDivision :: a -> Int -> a
     mean :: [a] -> a
+    mean as = meanDivision (foldr meanSum meanZero as) (length as)
 
 instance Fractional a => Meanable a where
-    mean [] = 0
-    mean as = (sum as) / (fromIntegral $ length as)
+    meanZero = 0
+    meanSum a b = a + b
+    meanDivision a 0 = 0
+    meanDivision a i = a / (fromIntegral i)
 
 instance (Meanable a) => Meanable [a] where
+    meanZero = []
+    meanSum [] as = as
+    meanSum as [] = as
+    meanSum as bs = map (\(a,b) -> meanSum a b) (zip as bs)
+    meanDivision a i = map (\a' -> meanDivision a' i) a
     mean as = map mean $ foldr (\a b -> map (\(a,b)->a:b) (zip a b)) (map (\a -> [a]) (head as)) (tail as)
 
 constPredictor :: y -> String -> Predictor t y
