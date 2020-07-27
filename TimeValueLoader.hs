dae85d33dcd2ea1d97c891fe02a2e675b8f6b87e
Hume2 <teratux.mail@gmail.com>
2020-07-27 16:09:32 +0200

    add TimeValueLoader

---
+++ TimeValueLoader.hs
@@ -0,0 +1,9 @@
+module TimeValueLoader where
+
+import Scenario
+
+timeValueLoader :: (Read t, Read v) => DataLoader t v
+timeValueLoader filename = do
+    content <- readFile filename;
+    let ls = lines content
+    return $ map ((\[t,v] -> (read t, read v)) . words) ls;
