ea6579a8951f3763be95341f46fad52bedc0bf81
Hume2 <teratux.mail@gmail.com>
2020-07-29 11:20:54 +0200

    add type CellGrid

--- Cell.hs
+++ Cell.hs
@@ -1,6 +1,7 @@
 module Cell where
 
 type Cell x = ([x], [x])
+type CellGrid x = (Cell x, [x])
 
 cellCentre :: RealFrac x => Cell x -> [x]
 cellCentre (pos, size) = map (\(p,s) -> p+s/2) (zip pos size)
@@ -8,23 +9,23 @@ cellCentre (pos, size) = map (\(p,s) -> p+s/2) (zip pos size)
 cellsIn :: (RealFrac x, Integral n) => x -> x -> n
 cellsIn size_total size_subcell = -floor (-size_total / size_subcell)
 
-subcellCount :: (RealFrac x, Integral n) => Cell x -> [x] -> n
-subcellCount (pos, size_total) size_subcell = product $ map (\(st, sc) -> cellsIn st sc) (zip size_total size_subcell)
+subcellCount :: (RealFrac x, Integral n) => CellGrid x -> n
+subcellCount ((pos, size_total), size_subcell) = product $ map (\(st, sc) -> cellsIn st sc) (zip size_total size_subcell)
 
-getSubcell :: (RealFrac x) => Cell x -> [x] -> [x] -> Cell x
-getSubcell (cell_pos, _) size_subcell my_pos = (map (\(a,b) -> a+b) (zip cell_pos rpos), size_subcell)
+getSubcell :: (RealFrac x) => CellGrid x -> [x] -> Cell x
+getSubcell ((cell_pos, _), size_subcell) my_pos = (map (\(a,b) -> a+b) (zip cell_pos rpos), size_subcell)
     where my_pos' = map (\(a,b) -> a-b) (zip my_pos cell_pos)
           rpos = map (\(sc, mp) -> (fromIntegral $ floor (mp/sc))*sc) (zip size_subcell my_pos')
 
-getSubcellId :: (RealFrac x, Integral n) => Cell x -> [x] -> [x] -> n
-getSubcellId (cell_pos, size_total) size_subcell my_pos = foldr (\(cc, cp) r -> cp+cc*r) 0 foo
+getSubcellId :: (RealFrac x, Integral n) => CellGrid x -> [x] -> n
+getSubcellId ((cell_pos, size_total), size_subcell) my_pos = foldr (\(cc, cp) r -> cp+cc*r) 0 foo
     where sc_counts = map (\(sc, st) -> floor(st/sc)) (zip size_subcell size_total)
           my_pos' = map (\(a,b) -> a-b) (zip my_pos cell_pos)
           my_pos'' = map (\(sc, mp) -> floor(mp/sc)) (zip size_subcell my_pos')
           foo = zip sc_counts my_pos''
 
-getSubcellById :: (RealFrac x, Integral n) => Cell x -> [x] -> n -> Cell x
-getSubcellById (cell_pos, size_total) size_subcell my_id = (map (\((a,b),c) -> (fromIntegral a)*b + c) (zip (zip log_pos size_subcell) cell_pos), size_subcell)
+getSubcellById :: (RealFrac x, Integral n) => CellGrid x -> n -> Cell x
+getSubcellById ((cell_pos, size_total), size_subcell) my_id = (map (\((a,b),c) -> (fromIntegral a)*b + c) (zip (zip log_pos size_subcell) cell_pos), size_subcell)
     where sc_counts = map (\(sc, st) -> floor(st/sc)) (zip size_subcell size_total)
           (log_pos, _) = foldl (\(lp, i) scc -> (lp ++ [i `mod` scc], i `div` scc)) ([], my_id) sc_counts
 
