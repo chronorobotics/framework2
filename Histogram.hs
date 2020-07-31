141e604cf864b087c62861d19917e9c9975c07f7
Hume2 <teratux.mail@gmail.com>
2020-07-31 11:18:40 +0200

    Histogram: use sequence instead of list

--- Histogram.hs
+++ Histogram.hs
@@ -1,6 +1,7 @@
 module Histogram where
 
 import Data.Fixed
+import Data.Sequence
 import Predictor
 import Mean
 
@@ -8,15 +9,14 @@ getBinNo :: (RealFrac t, Integral n) => t -> t -> n -> n
 getBinNo period time nbins = floor ((mod' time period) / bin_length)
     where bin_length = period / (fromIntegral nbins)
 
-histogramPredictor :: (RealFrac t, Show y) => [y] -> t -> String -> Predictor t y
+histogramPredictor :: (RealFrac t, Show y) => Seq y -> t -> String -> Predictor t y
 histogramPredictor bins period str = (str, pred)
-    where pred t = bins !! (getBinNo period t (length bins))
+    where pred t = index bins (getBinNo period t (Data.Sequence.length bins))
 
 histogramMethod :: (RealFrac t, Show y, Meanable y) => MethodWithParams (t, Int) t y h
-histogramMethod (period, nbins) (_, dat) = histogramPredictor (map (\(s,n) -> meanDivision s n) bins) period ("Hist "++(show nbins))
-    where s = replicate nbins (meanZero, 0)
-          addToBin bs n y = b1 ++ [add b y] ++ b2
-              where (b1, (b:b2)) = splitAt n bs
-                    add (s, n) y = (meanSum s y, n + 1)
+histogramMethod (period, nbins) (_, dat) = histogramPredictor (fmap (\(s,n) -> meanDivision s n) bins) period ("Hist "++(show nbins))
+    where s = Data.Sequence.replicate nbins (meanZero, 0)
+          addToBin bs i y = update i (add (index bs i) y) bs
+              where add (s, n) y = (meanSum s y, n + 1)
           bins = foldr (\(t, y) bs -> addToBin bs (getBinNo period t nbins) y) s dat
 
