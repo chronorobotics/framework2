5ef4319625716dc278c6fbc5da7a75585245e62d
Hume2 <teratux.mail@gmail.com>
2020-07-31 11:19:05 +0200

    CellGridMethod: use Vector instead of Seq

--- CellGridMethod.hs
+++ CellGridMethod.hs
@@ -3,19 +3,20 @@ module CellGridMethod where
 import Predictor
 import Cell
 import Data.Sequence
+import Data.Vector
 
 cellGridMethod :: RealFrac x => (h -> Int -> h') -> Method t y h' -> Method (t, [x]) y (CellGrid x, h)
 cellGridMethod hf m = cgm
     where cgm ((grid, rest), dat) = (name, pred)
               where cc = subcellCount grid
-                    pred (t, x) = (index preds (getSubcellId grid x)) t
-                    preds' = fmap (\i -> m (hf rest i, index dat' i)) $ fromList [0..((subcellCount grid) - 1)]
+                    pred (t, x) = (preds ! (getSubcellId grid x)) t
+                    preds' = Prelude.map (\i -> m (hf rest i, index dat' i)) [0..((subcellCount grid) - 1)]
                     dat0 = Data.Sequence.replicate cc []
-                    add ((t, x), y) d = update i ((t, y) : (index d i)) d
+                    add ((t, x), y) d = Data.Sequence.update i ((t, y) : (index d i)) d
                         where i = getSubcellId grid x
-                    dat' = foldr add dat0 dat
-                    (name, _) = index preds' 0
-                    preds = fmap (\(_, p) -> p) preds'
+                    dat' = Prelude.foldr add dat0 dat
+                    (name, _) = preds' !! 0
+                    preds = Data.Vector.fromList $ Prelude.map (\(_, p) -> p) preds'
 
 cellGridMethodParams :: RealFrac x => (h -> Int -> h') -> MethodWithParams p t y h' -> MethodWithParams p (t, [x]) y (CellGrid x, h)
 cellGridMethodParams hf mwp = parametriseMethodTransform (cellGridMethod hf) mwp
