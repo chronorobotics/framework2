d4c447b06f6d4fefe91c3d0981c4f992d7f67bd1
Hume2 <teratux.mail@gmail.com>
2020-08-28 13:22:16 +0200

    split Distribution from EM algorithm

---
+++ Distribution.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+module Distribution where
+
+import Predictor
+import System.Random
+
+class Distribution dist space | dist -> space where
+    densityAt :: RealFloat f => dist -> space -> f
+
+type InitDistribution g d = g -> (d, g)
+type DistributionEstimator s d = (String, [s] -> d)
+type DistributionToMethod t y h d = DistributionEstimator t d -> Method t y h
+
+-- mixture of distributions
+instance (Distribution d s, RealFloat f) => Distribution [(f, d)] s where
+    densityAt wcs p = realToFrac $ sum $ map (\(w, c) -> w * densityAt c p) wcs
+
+-- cartesian product of distributions
+instance (Distribution d1 s1, Distribution d2 s2) => Distribution (d1, d2) (s1, s2) where
+    densityAt (d1, d2) (p1, p2) = (densityAt d1 p1) * (densityAt d2 p2)
+
+logLikelihood :: (RealFloat f, Distribution d s) => [s] -> d -> f
+logLikelihood points dist = sum $ map (\p -> log $ densityAt dist p) points
+
+mergeInits :: RandomGen g => InitDistribution g d1 -> InitDistribution g d2 -> InitDistribution g (d1, d2)
+mergeInits init1 init2 gen = ((d1, d2), gen'')
+    where (d1, gen') = init1 gen
+          (d2, gen'') = init2 gen'
+
+predictByDist :: (Distribution d s, RealFloat f) => [f] -> [(f, d)] -> String -> s -> f
+predictByDist centres dists name at = (sum $ zipWith (*) centres wds) / (sum wds)
+    where ws = map fst dists
+          ds = map (\(_, d) -> densityAt d at) dists
+          wds = zipWith (*) ws ds
+
--- EMAlgorithm.hs
+++ EMAlgorithm.hs
@@ -5,31 +5,17 @@
 {-# LANGUAGE UndecidableInstances #-}
 module EMAlgorithm where
 
+import Distribution
 import System.Random
-
-class Distribution dist space | dist -> space where
-    densityAt :: RealFloat f => dist -> space -> f
 
 class (Distribution dist space) => EMDistribution dist space | dist -> space where
     maximumLikelihoodEstimate :: RealFloat f => [(space, f)] -> dist
 
-type InitDistribution g d = g -> (d, g)
-
--- mixture of distributions
-instance (Distribution d s, RealFloat f) => Distribution [(f, d)] s where
-    densityAt wcs p = realToFrac $ sum $ map (\(w, c) -> w * densityAt c p) wcs
-
 -- cartesian product of distributions
-instance (Distribution d1 s1, Distribution d2 s2) => Distribution (d1, d2) (s1, s2) where
-    densityAt (d1, d2) (p1, p2) = (densityAt d1 p1) * (densityAt d2 p2)
-
 instance (EMDistribution d1 s1, EMDistribution d2 s2) => EMDistribution (d1, d2) (s1, s2) where
     maximumLikelihoodEstimate dat = (maximumLikelihoodEstimate dat1, maximumLikelihoodEstimate dat2)
         where dat1 = map (\((p1, _), w) -> (p1, w)) dat
               dat2 = map (\((_, p2), w) -> (p2, w)) dat
-
-logLikelihood :: (RealFloat f, Distribution d s) => [s] -> d -> f
-logLikelihood points dist = sum $ map (\p -> log $ densityAt dist p) points
 
 expectation :: (RealFloat f, EMDistribution d s) => [s] -> [(f, d)] -> [[f]]
 expectation points clusters = map e1 points
@@ -60,8 +46,10 @@ initEM init n gen = (map (\(w, c) -> (w/sum_w, c)) wcs, gen')
           (wcs, gen') = iterate add_wc ([], gen) !! n
           sum_w = sum $ map (\(w, _) -> w) wcs
 
-mergeInits :: RandomGen g => InitDistribution g d1 -> InitDistribution g d2 -> InitDistribution g (d1, d2)
-mergeInits init1 init2 gen = ((d1, d2), gen'')
-    where (d1, gen') = init1 gen
-          (d2, gen'') = init2 gen'
+emAlgorithm' :: (RealFloat f, EMDistribution d s) => [(f, d)] -> f -> (d -> d) -> DistributionEstimator s [(f, d)]
+emAlgorithm' start min_dl bounder = ("EM", performEM start min_dl bounder)
+
+emAlgorithm :: (Random f, RealFloat f, EMDistribution d s) => Int -> Int -> InitDistribution StdGen d -> f -> (d -> d) -> DistributionEstimator s [(f, d)]
+emAlgorithm clusters seed init = emAlgorithm' start
+    where (start, _) = initEM init clusters $ mkStdGen seed
 
