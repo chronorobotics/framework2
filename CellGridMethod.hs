c26418f1eb6a2274ed96535a153b99ef162e554d
Hume2 <teratux.mail@gmail.com>
2020-09-09 21:01:16 +0200

    CellGridMethod: split dataset more effectively

--- CellGridMethod.hs
+++ CellGridMethod.hs
@@ -2,8 +2,7 @@ module CellGridMethod where
 
 import Predictor
 import Cell
-import Data.Sequence
-import Data.Vector
+import Data.Vector ((!), fromList)
 
 -- |This function transforms a method to a method with larger input dimension so
 -- the input space is split into cells and the given method is used for each
@@ -15,13 +14,10 @@ cellGridMethod hf m = cgm
     where cgm ((grid, rest), dat) = (name, pred)
               where cc = subcellCount grid
                     pred (t, x) = (preds ! (getSubcellId grid x)) t
-                    preds' = Prelude.map (\i -> m (hf rest i, index dat' i)) [0..((subcellCount grid) - 1)]
-                    dat0 = Data.Sequence.replicate cc []
-                    add ((t, x), y) d = Data.Sequence.update i ((t, y) : (index d i)) d
-                        where i = getSubcellId grid x
-                    dat' = id $! Prelude.foldr add dat0 dat
+                    preds' = map (\i -> m (hf rest i, map (\(_, t, y) -> (t, y)) $ filter (\(c, t, y) -> c == i) dat')) [0..((subcellCount grid) - 1)]
+                    dat' = map (\((t, x), y) -> (getSubcellId grid x, t, y)) dat
                     (name, _) = preds' !! 0
-                    preds = Data.Vector.fromList $ Prelude.map snd $! preds'
+                    preds = Data.Vector.fromList $ map snd $! preds'
 
 -- |This function does the same as cellGridMethod but it applies to a method
 --  with parameters. The same parameters are being passed to each cell.
